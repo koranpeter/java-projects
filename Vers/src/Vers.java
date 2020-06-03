1f8491a8aacafd85a5891ef9051275a1003fcd6f
Korán Péter <koranpeter01@gmail.com>
2020-06-03 19:43:52 +0200

    initial commit

---
+++ Vers/src/Vers.java
@@ -0,0 +1,40 @@
+
+public class Vers {
+	public static void main(String[] args) {
+		System.out.println("Petőfi Sándor");
+		System.out.println();
+		System.out.println("FÖLTÁMADOTT A TENGER...");
+		System.out.println();
+		System.out.println("Föltámadott a tenger,");
+		System.out.println("A népek tengere;");
+		System.out.println("Ijesztve eget-földet,");
+		System.out.println("Szilaj hullámokat vet");
+		System.out.println("Rémítő ereje.");
+		System.out.println();
+		System.out.println("Látjátok ezt a táncot?");
+		System.out.println("Halljátok e zenét?");
+		System.out.println("Akik még nem tudtátok,");
+		System.out.println("Most megtanulhatjátok,");
+		System.out.println("Hogyan mulat a nép.");
+		System.out.println("");
+		System.out.println("Reng és üvölt a tenger,");
+		System.out.println("Hánykódnak a hajók,");
+		System.out.println("Sűlyednek a pokolra,");
+		System.out.println("Az árboc és vitorla");
+		System.out.println("Megtörve, tépve lóg.");
+		System.out.println("");
+		System.out.println("Tombold ki, te özönvíz,");
+		System.out.println("Tombold ki magadat,");
+		System.out.println("Mutasd mélységes medred,");
+		System.out.println("S dobáld a fellegekre");
+		System.out.println("Bőszült tajtékodat;");
+		System.out.println("");
+		System.out.println("Jegyezd vele az égre");
+		System.out.println("Örök tanúságúl:");
+		System.out.println("Habár fölűl a gálya,");
+		System.out.println("S alúl a víznek árja,");
+		System.out.println("Azért a víz az úr!");
+
+	
+	}
+}
